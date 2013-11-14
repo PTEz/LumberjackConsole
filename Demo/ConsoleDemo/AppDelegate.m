8008c81d8cc7b4cb82198155e96d893a7b4ef606
利辺羅エルネスト <rivera_ernesto@cyberagent.co.jp>
2013-11-14 16:31:52 +0900

    Update Demo

--- Demo/ConsoleDemo/AppDelegate.m
+++ Demo/ConsoleDemo/AppDelegate.m
@@ -2,8 +2,20 @@
 //  AppDelegate.m
 //  ConsoleDemo
 //
-//  Created by 利辺羅 on 2013/11/13.
-//  Copyright (c) 2013年 PTEz. All rights reserved.
+//  Created by Ernesto Rivera on 2013/11/13.
+//  Copyright (c) 2013.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 #import "AppDelegate.h"
@@ -15,10 +27,12 @@ static int ddLogLevel = LOG_LEVEL_VERBOSE;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Add the console dashboard
+#ifndef MY_PRODUCTION_MACRO
+    // Add the console dashboard for testing builds
     [DDLog addLogger:[PTEDashboard sharedDashboard].logger];
     
     DDLogInfo(@"Added console dashboard");
+#endif
     
     [self generateRandomLogMessage];
     
